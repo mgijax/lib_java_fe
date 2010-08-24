c8c07a48185070b327a30503b881b3ab19f61895
Matt Hall <Matt.Hall@jax.org>
2010-08-24 18:47:33 +0000

    Initial version of the fe lib product

---
+++ java/org/jax/mgi/shr/fe/IndexConstants.java
@@ -0,0 +1,8 @@
+package org.jax.mgi.shr.fe;
+
+public class IndexConstants {
+    public static final String SEQUENCE_ID      = "accID";
+    public static final String MARKER_KEY       = "markerKey";
+    public static final String SEQUENCE_KEY     = "sequenceKey";
+    public static final String REFERENCE_KEY    = "referenceKey";
+}
