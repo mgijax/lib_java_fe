5a1bf15f6081e62737db04c2419fa3bd31ab648b
Matt Hall <Matt.Hall@jax.org>
2010-08-25 15:49:07 +0000

    Changed my naming conventions to match that of the front end constants.

--- java/org/jax/mgi/shr/fe/IndexConstants.java
+++ java/org/jax/mgi/shr/fe/IndexConstants.java
@@ -1,8 +1,9 @@
 package org.jax.mgi.shr.fe;
 
 public class IndexConstants {
-    public static final String SEQUENCE_ID      = "accID";
-    public static final String MARKER_KEY       = "markerKey";
-    public static final String SEQUENCE_KEY     = "sequenceKey";
-    public static final String REFERENCE_KEY    = "referenceKey";
+    public static final String SEQ_ID       = "accID";
+    public static final String MRK_KEY      = "markerKey";
+    public static final String SEQ_KEY      = "sequenceKey";
+    public static final String REF_KEY      = "referenceKey";
+    public static final String REF_ID       = "accID";
 }
