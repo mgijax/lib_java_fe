e423686f2a98c13b9a7b2a7ea75ef5e6b7998707
Matt Hall <Matt.Hall@jax.org>
2010-08-26 13:21:27 +0000

    *** empty log message ***

--- java/org/jax/mgi/shr/fe/IndexConstants.java
+++ java/org/jax/mgi/shr/fe/IndexConstants.java
@@ -1,9 +1,42 @@
 package org.jax.mgi.shr.fe;
 
 public class IndexConstants {
+    
+    // Sequence Stuff
     public static final String SEQ_ID       = "accID";
-    public static final String MRK_KEY      = "markerKey";
     public static final String SEQ_KEY      = "sequenceKey";
-    public static final String REF_KEY      = "referenceKey";
-    public static final String REF_ID       = "accID";
+    
+    // Marker Stuff
+    public static final String MRK_KEY      = "markerKey";
+    
+    // Reference Stuff
+    public static final String REF_KEY              = "referenceKey";
+    public static final String REF_ID               = "accID";
+    public static final String REF_JOURNAL          = "accID";
+    public static final String REF_TITLE            = "title";
+    public static final String REF_YEAR             = "year";
+    public static final String REF_ABSTRACT         = "abstract";
+    public static final String REF_ISSUE            = "issue";
+    public static final String REF_VOLUME           = "volume";
+    public static final String REF_FIRST_AUTHOR     = "volume";
+    public static final String REF_LAST_AUTHOR      = "volume";
+    public static final String REF_AUTHOR           = "author";
+    public static final String REF_AUTHOR_FORMATTED = "authorsFormatted";
+    
+    // General stuff
+    public static final String JNUM_ID          = "jnumID";
+    public static final String PUBMED_ID        = "pubMedID";
+    
+    // Counts
+    public static final String PRB_COUNT        = "probe_count";
+    public static final String MRK_COUNT        = "marker_count";
+    public static final String ALL_COUNT        = "alleleCount";
+    public static final String SEQ_COUNT        = "sequenceCount";
+    public static final String ORTHO_COUNT      = "orthologCount";
+    public static final String MAP_EXPT_COUNT   = "mappingExptCount";
+    public static final String GXD_INDEX_COUNT  = "gxdIndexCount";
+    public static final String GXD_RESULT_COUNT = "gxdResultCount";
+    public static final String GXD_STRUCT_COUNT = "gxdStructureCount";
+    public static final String GXD_ASSAY_COUNT  = "gxdAssayCount";
+    
 }
