7e0820a23bb5ce760b6985bb5ef31bedcb295013
Matt Hall <Matt.Hall@jax.org>
2010-09-03 16:12:02 +0000

    *** empty log message ***

--- java/org/jax/mgi/shr/fe/IndexConstants.java
+++ java/org/jax/mgi/shr/fe/IndexConstants.java
@@ -12,15 +12,17 @@ public class IndexConstants {
     // Reference Stuff
     public static final String REF_KEY              = "referenceKey";
     public static final String REF_ID               = "accID";
-    public static final String REF_JOURNAL          = "accID";
+    public static final String REF_JOURNAL          = "journal";
+    public static final String REF_JOURNAL_SORT     = "journalSort";
     public static final String REF_TITLE            = "title";
     public static final String REF_YEAR             = "year";
     public static final String REF_ABSTRACT         = "abstract";
     public static final String REF_ISSUE            = "issue";
     public static final String REF_VOLUME           = "volume";
-    public static final String REF_FIRST_AUTHOR     = "volume";
-    public static final String REF_LAST_AUTHOR      = "volume";
+    public static final String REF_FIRST_AUTHOR     = "firstAuthor";
+    public static final String REF_LAST_AUTHOR      = "lastAuthor";
     public static final String REF_AUTHOR           = "author";
+    public static final String REF_AUTHOR_SORT      = "authorSort";
     public static final String REF_AUTHOR_FORMATTED = "authorsFormatted";
     
     // General stuff
