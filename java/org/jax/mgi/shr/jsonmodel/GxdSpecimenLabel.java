cd18d74b8178b9398dc10dcc130b732c9de86fdc
Kevin Stone <kevinrstone@gmail.com>
2016-03-31 14:08:44 -0400

    adding json models for GXD Image Pane summary

---
+++ java/org/jax/mgi/shr/jsonmodel/GxdSpecimenLabel.java
@@ -0,0 +1,48 @@
+package org.jax.mgi.shr.jsonmodel;
+
+
+/**
+ * Meta information about image specimen label
+ */
+public class GxdSpecimenLabel {
+	
+	private String label;
+	private String assayId;
+	
+	
+	public String getLabel() {
+		return label;
+	}
+	public String getAssayId() {
+		return assayId;
+	}
+	public void setLabel(String label) {
+		this.label = label;
+	}
+	public void setAssayId(String assayId) {
+		this.assayId = assayId;
+	}
+	@Override
+	public String toString() {
+		return "GxdSpecimenLabel [label=" + label + ", assayId=" + assayId
+				+ "]";
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		GxdSpecimenLabel other = (GxdSpecimenLabel) obj;
+		
+		if (assayId == null) {
+			if (other.assayId != null)
+				return false;
+		} else if (!assayId.equals(other.assayId))
+			return false;
+		if (label == null) {
+			if (other.label != null)
+				return false;
+		} else if (!label.equals(other.label))
+			return false;
+		return true;
+	}
+
+}
