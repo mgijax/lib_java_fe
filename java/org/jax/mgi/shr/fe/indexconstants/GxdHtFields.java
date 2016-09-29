30f5dac97048393315ce127f77afd0333b54f786
Jonathan Beal <jsb@jax.org>
2016-09-29 12:08:27 -0400

    initial addition

---
+++ java/org/jax/mgi/shr/fe/indexconstants/GxdHtFields.java
@@ -0,0 +1,38 @@
+package org.jax.mgi.shr.fe.indexconstants;
+
+
+/**
+ * Fields specific to indices for high-throughput expression data
+ */
+public class GxdHtFields 
+{
+	// experiment fields
+	public static String EXPERIMENT_KEY = "experimentKey";
+	public static String ARRAYEXPRESS_ID = "arrayExpressID";
+	public static String GEO_ID = "geoID";
+	public static String METHOD = "method";
+	public static String TITLE = "title";
+	public static String DESCRIPTION = "description";
+	public static String SAMPLE_COUNT = "sampleCount";
+	public static String EXPERIMENTAL_VARIABLES = "experimentalVariables";
+	public static String STUDY_TYPE = "studyType";
+	public static String NOTE = "note";
+	public static String BY_DEFAULT = "byDefault";
+	public static String TITLE_DESCRIPTION = "titleDescription";
+	public static String REFERENCE_ID = "referenceID";
+	public static String EXPERIMENT_ID = "experimentID";
+
+	// sample fields
+	public static String SAMPLE_KEY = "sampleKey";
+	public static String STRUCTURE_TERM = "structureTerm";
+	public static String STRUCTURE_ID = "structureID";
+	public static String THEILER_STAGE = "theilerStage";
+	public static String AGE = "age";
+	public static String SEX = "sex";
+	public static String NAME = "name";
+	public static String ORGANISM = "organism";
+	public static String MUTANT_ALLELES = "mutantAlleles";
+	public static String GENETIC_BACKGROUND = "geneticBackground";
+	public static String STRUCTURE_SEARCH = "structureSearch";
+	public static String MUTATED_GENE = "mutatedGene";
+}
