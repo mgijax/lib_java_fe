902f256ea7a92247b229013e66e301d5bd0b79ee
Joel Richardson <joel.richardson@jax.org>
2025-05-28 14:51:17 -0400

    e4g-245 Cell Type Filter: add to the GXD HT Experiment Summary

--- src/main/java/org/jax/mgi/shr/fe/indexconstants/GxdHtFields.java
+++ src/main/java/org/jax/mgi/shr/fe/indexconstants/GxdHtFields.java
@@ -35,6 +35,7 @@ public class GxdHtFields
 	public static String CELLTYPE_ID = "celltypeID";
 	public static String CT_SEARCH_IDS = "ctSearchIDs";
 	public static String CT_SEARCH_TERMS = "ctSearchTerms";
+	public static String CT_FACET_TERMS = "ctFacetTerms";
 	public static String AGE = "age";
 	public static String AGE_MIN = "ageMin";
 	public static String AGE_MAX = "ageMax";
