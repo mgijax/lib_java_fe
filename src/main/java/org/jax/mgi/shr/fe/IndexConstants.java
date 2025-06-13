861561b749dada8fb744954e5bcd17f2e7d4570f
Joel Richardson <joel.richardson@jax.org>
2025-06-13 10:16:34 -0400

    e4g-176 Add a Cell Type Filter in the Quick Search vocab bucket tab

--- src/main/java/org/jax/mgi/shr/fe/IndexConstants.java
+++ src/main/java/org/jax/mgi/shr/fe/IndexConstants.java
@@ -416,6 +416,7 @@ public class IndexConstants {
 	public static final String QS_PHENOTYPE_FACETS = "phenotypeFacets";
 	public static final String QS_MARKER_TYPE_FACETS = "markerTypeFacets";
 	public static final String QS_EXPRESSION_FACETS = "expressionFacets";
+	public static final String QS_CELL_TYPE_FACETS = "cellTypeFacets";
 	
 	public static final String QS_ATTRIBUTES = "attributes";			// special for strain bucket
 	public static final String QS_REFERENCE_COUNT = "referenceCount";
